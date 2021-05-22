dd2002b70223b2a54f56bb5d607d6a0a46bfe890
hyq <458941968@qq.com>
2021-05-22 16:13:29 +0800

    add configure x86 arm

---
+++ src/pjproject-2.10/configure_arm.sh
@@ -0,0 +1,7 @@
+./configure \
+CC=arm-linux-gcc \
+CROSS_COMPILE=arm-none-linux-gnueabi- \
+--build=i386-linux \
+--prefix=/home/hyq/workspace/mini2440/hyqthirdlib/install/pjproject_arm \
+--host=arm-none-linux-gnueabi \
+--target=arm-none-linux-gnueabi
---
+++ src/pjproject-2.10/configure_x86.sh
@@ -0,0 +1,1 @@
+./configure --prefix=/home/hyq/workspace/mini2440/hyqthirdlib/install/pjproject_x86
