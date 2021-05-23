7cf474f02000e113b53fa53ffc3fadfbefab63b4
hyq <458941968@qq.com>
2021-05-23 14:46:18 +0800

    add pjproject

--- src/pjproject-2.10/configure_arm.sh
+++ src/pjproject-2.10/configure_arm.sh
@@ -2,6 +2,7 @@
 CC=arm-linux-gcc \
 CROSS_COMPILE=arm-none-linux-gnueabi- \
 --build=i386-linux \
---prefix=/home/hyq/workspace/mini2440/hyqthirdlib/install/pjproject_arm \
+--prefix=/home/hyq/workspace/mini2440/mini2440-app/install/pjproject \
 --host=arm-none-linux-gnueabi \
---target=arm-none-linux-gnueabi
+--target=arm-none-linux-gnueabi \
+--disable-libyuv --disable-libwebrtc --enable-shared
